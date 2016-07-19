b8699fd5fa820d02db24a82749db404eb2fa2c36
3792274 <3792274@qq.com>
2016-07-19 19:37:12 +0800

    s

--- 第5章面向对象(上)/chapter05_01_类和对象/DogTest.java
+++ 第5章面向对象(上)/chapter05_01_类和对象/DogTest.java
@@ -1,21 +1,18 @@
 package chapter05_01_类和对象;
 
-
-
 /**
- * Description:
- * <br/>网站: <a href="http://www.crazyit.org">疯狂Java联盟</a>
- * <br/>Copyright (C), 2001-2016, Yeeku.H.Lee
- * <br/>This program is protected by copyright laws.
- * <br/>Program Name:
- * <br/>Date:
+ * Description: <br/>
+ * 网站: <a href="http://www.crazyit.org">疯狂Java联盟</a> <br/>
+ * Copyright (C), 2001-2016, Yeeku.H.Lee <br/>
+ * This program is protected by copyright laws. <br/>
+ * Program Name: <br/>
+ * Date:
+ * 
  * @author Yeeku.H.Lee kongyeeku@163.com
  * @version 1.0
  */
-public class DogTest
-{
-	public static void main(String[] args)
-	{
+public class DogTest {
+	public static void main(String[] args) {
 		// 创建Dog对象
 		Dog dog = new Dog();
 		// 调用Dog对象的run()方法
