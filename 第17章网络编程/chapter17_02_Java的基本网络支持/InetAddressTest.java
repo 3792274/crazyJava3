fafcf84438804786b30c538300f63394745d4d01
3792274 <3792274@qq.com>
2016-07-21 10:27:31 +0800

    0

--- 第17章网络编程/chapter17_02_Java的基本网络支持/InetAddressTest.java
+++ 第17章网络编程/chapter17_02_Java的基本网络支持/InetAddressTest.java
@@ -17,13 +17,17 @@ public class InetAddressTest {
 	public static void main(String[] args) throws Exception {
 		// 根据主机名来获取对应的InetAddress实例
 		InetAddress ip = InetAddress.getByName("www.baidu.com");
+		
 		// 判断是否可达
 		System.out.println("BaiDu是否可达：" + ip.isReachable(2000));
+	
 		// 获取该InetAddress实例的IP字符串
 		System.out.println(ip.getHostAddress());
+	
 		// 根据原始IP地址来获取对应的InetAddress实例
 		InetAddress local = InetAddress.getByAddress(new byte[] { 127, 0, 0, 1 });
 		System.out.println("本机是否可达：" + local.isReachable(5000));
+		
 		// 获取该InetAddress实例对应的全限定域名
 		System.out.println(local.getCanonicalHostName());
 	}
